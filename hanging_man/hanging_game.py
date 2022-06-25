08308c67f7b007965df91a1bdfc15c2dd271006e
“pc_and3” <and3.sci@gmail.com>
2022-06-24 23:14:20 -0500

    version v.1.0 hanging_proyectman

--- hanging_man/hanging_game.py
+++ hanging_man/hanging_game.py
@@ -1,5 +1,130 @@
+from pyfiglet import Figlet
+from termcolor import colored, cprint
+import os 
+
+p_red = lambda x: cprint(x, "red") # 'on_yellow', attrs=['bold']
+p_green = lambda x: cprint(x, "green")
+p_blue = lambda x: cprint(x, "blue")
+p_orange = lambda x: cprint(x, "yellow")
+p_title = lambda x: cprint(x, "green", attrs=['bold'])
+p_sub_red = lambda x: cprint(x, "white", "on_red", attrs=['bold', "underline"])
+
+# https://1lineart.kulaone.com/#/
+b_0 = """
+   ______
+   |    |
+   |  
+   |  
+  / \  
+"""
+b_1 = """
+   ______
+   |    |
+   |  (̾●̮̮̃̾•̃̾)
+   |  
+  / \  
+"""
+b_2 = """
+   ______
+   |    |
+   |  (̾●̮̮̃̾•̃̾)
+   |    |
+  / \  
+"""
+b_4 = """
+   ______
+   |    |
+   |  (̾●̮̮̃̾•̃̾)
+   |  --|--
+  / \  
+"""
+b_3 = """
+   ______
+   |    |
+   |  (̾●̮̮̃̾•̃̾)
+   |  --|
+  / \  
+"""
+b_5 = """
+   ______
+   |    |
+   |  (- ̮̮̃-̃)
+   |  --|--
+  / \  / \    Has muerto!!!
+"""
+body = [b_0, b_1 ,b_2, b_3, b_4, b_5]
+
+
+
+def check_input():
+   # input open 
+    true_input = False
+    while true_input == False: 
+      shot = input("ingresa una letra: ")
+      if len(shot) != 1:
+        print("solo se permite una letra de entrada!") 
+      elif not shot.isalpha():
+        print("solo se permiten letras de entrada! ")
+      else: 
+        true_input = True
+        return shot
+
+def show_function(advance, lifes, score):
+    if lifes > 0:
+        print(body[5-lifes])
+    else: 
+       p_red(body[5-lifes])
+    concat_advance = ""
+    for l in advance:
+        concat_advance += l
+        concat_advance += " "
+    p_orange(concat_advance)
+    
+    p_green("lifes: " + lifes * "♡")
+    
+    p_blue("score: " + str(sum(score)))
+
+def tablero(word):
+  l_word = list(word)
+  lifes = 5
+  size = len(l_word)
+  score = []
+  for i in range(size):
+    score.append(0)
+  advance = list("_"*size)
+  show_function(advance, lifes, score)
+  p_blue("adivina la palabra oculta, tiene " + str(size) + " letras")
+  while True:
+    
+    shot = check_input() # input check function 
+    if shot in l_word:
+      for i_, lt in enumerate(l_word):
+        if lt == shot:
+          advance[i_] = shot
+          score[i_] = 1
+    else:
+      lifes -= 1
+    os.system("clear")
+    show_function(advance, lifes, score)
+    
+
+    if lifes == 0:
+      p_orange("la palabra era:" + str(word))
+      break
+    if sum(score) == size:
+        f = Figlet(font='standard')
+        p_orange(f.renderText('Has ganado!'))
+        break
+
 
 def run ():
-    pass
+    f = Figlet(font='standard') #slant, banner3-D, isometric2
+    p_title(f.renderText('juego del ahorcado'))
+    p_sub_red("by Andres Gutierrez Castillo. Mex. 2022")
+
+    word = "hu" 
+    tablero(word)
+
+
 if __name__ == "__main__": 
     run()
---
+++ hanging_man/preProcessData.py
@@ -0,0 +1,92 @@
+import re
+
+# "how to split a text in words"
+def split_words():
+  with open("./data/data.txt", "r", encoding="utf-8") as f: # dir of the paragraph with the text to extract the words
+    for line in f:
+      words = line.split(" ")
+  print(words)
+  return words 
+# i clear words with accent i searched "how to change a character for another in python "
+def clear_accented_words(data):
+  cleaned_data = []
+  for word in data:
+    new_word = word.maketrans('áéíóú', 'aeiou')
+    c_word = word.translate(new_word)
+    cleaned_data.append(c_word)
+  print(cleaned_data)
+  return cleaned_data
+
+# i searched "Keep Only Letters From a String in Python"
+def get_just_letters(data):
+  cleaned_data = []
+  for word in data:
+    if "ñ" in word: # also i discard words with "ñ" i searched "Check if String Contains Substring in Python"
+      continue
+    else:
+      c_word = re.sub(r'[^a-zA-Z]', '', word) # regularExpresions.sub(simbolsToDetect, simbolToChange, data)
+      cleaned_data.append(c_word)
+  print(cleaned_data)
+  return cleaned_data
+
+# clean uppercase and keep only lowercase i searche "how to change uppercase to lowercase in python"
+def clear_uppercase(data):
+  cleaned_data = []
+  for word in data:
+    c_word = word.lower()
+    cleaned_data.append(c_word)
+  print(cleaned_data)
+  print(len(cleaned_data))
+
+  return cleaned_data
+# clear word with less than 4 words
+def clear_short_words(data):
+  cleaned_data = []
+  for word in data:
+    if len(word) <= 4:
+      continue
+    else:
+      cleaned_data.append(word)
+  print(cleaned_data)
+  print(len(cleaned_data))
+
+  return cleaned_data
+cleaned_data = clear_short_words(clear_uppercase(get_just_letters(clear_accented_words(split_words()))))
+# maybe could be better search "how to claer text with python"
+
+#---------------------------------------------------------------
+
+# lets analyse the data to get insights 
+def count_words(data):
+  info_words = {}
+  level_words = {}
+  words_sizes=[]
+  for word in data:
+    size = len(word)
+    words_sizes.append(size)
+
+  n_types = list(set(words_sizes))
+  for i in n_types:
+    info_words["words with "+str(i)+" letters"]= words_sizes.count(i)
+
+  for ty in n_types:
+    lis = []
+    for w in data:
+      if len(w) == ty:
+        lis.append(w)
+        level_words[len(w)] = lis
+
+  return info_words, level_words
+
+info_words, level_words = count_words(cleaned_data)
+
+for key, value in info_words.items():
+  print(key + "----"+ str(value))
+for key, value in level_words.items():
+  print(str(key) + "----"+ str(value))
+
+import csv
+
+with open("./data/dict_of_words.csv", "w", encoding="utf-8") as f: 
+    for key in level_words.keys():
+        f.write("%s, %s\n" % (key, level_words[key]))
